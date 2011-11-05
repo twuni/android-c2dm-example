5b32774de742687964cfe6c20c3d3d2842611d57
Leo Accend <github.aeonic@spamgourmet.com>
2011-11-04 21:00:23 -0700

    Displaying a status bar notification whenever a C2DM message is received.

--- src/org/twuni/android/c2dm/receiver/MessageReceiver.java
+++ src/org/twuni/android/c2dm/receiver/MessageReceiver.java
@@ -1,5 +1,12 @@
 package org.twuni.android.c2dm.receiver;
 
+import org.twuni.android.c2dm.activity.MainActivity;
+
+import android.R;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,15 +15,40 @@ import android.util.Log;
 
 public class MessageReceiver extends BroadcastReceiver {
 
-	@Override
-	public void onReceive( Context context, Intent intent ) {
-		Log.d( getClass().getSimpleName(), "Received C2DM Message." );
-		Bundle extras = intent.getExtras();
+	private void logExtras( Bundle extras ) {
+
 		Log.d( getClass().getSimpleName(), "{" );
+
 		for( String key : extras.keySet() ) {
 			Log.d( getClass().getSimpleName(), String.format( "  \"%s\": \"%s\"", key, extras.get( key ) ) );
 		}
+
 		Log.d( getClass().getSimpleName(), "}" );
+
+	}
+
+	@Override
+	public void onReceive( Context context, Intent intent ) {
+
+		Bundle extras = intent.getExtras();
+
+		logExtras( extras );
+
+		showNotification( context, R.drawable.stat_notify_voicemail, "C2DM Test Message", extras.getString( "message" ), MainActivity.class );
+
+	}
+
+	public void showNotification( Context context, int iconResourceId, String title, String content, Class<? extends Activity> activityToLaunch ) {
+
+		Notification notification = new Notification( iconResourceId, content, System.currentTimeMillis() );
+
+		PendingIntent pendingIntent = PendingIntent.getActivity( context, 0, new Intent( context, activityToLaunch ), PendingIntent.FLAG_UPDATE_CURRENT );
+
+		notification.setLatestEventInfo( context, title, content, pendingIntent );
+
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService( Context.NOTIFICATION_SERVICE );
+		notificationManager.notify( 1, notification );
+
 	}
 
 }
